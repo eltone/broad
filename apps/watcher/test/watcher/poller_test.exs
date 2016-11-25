6e699fde90e0572dba3ed88315e915c934653a5d
eltone <anthonyhobbs@gmail.com>
2016-11-25 18:23:01 +0000

    fix event test

--- apps/watcher/test/watcher/poller_test.exs
+++ apps/watcher/test/watcher/poller_test.exs
@@ -2,7 +2,8 @@ defmodule Watcher.PollerTest do
   use ExUnit.Case, async: true
 
   setup do
-    {:ok, _pid} = Watcher.Poller.start_link(["127.0.0.1:11300"])
+    {:ok, _pool_pid} = Watcher.Pool.start_link(:test_pool, ["127.0.0.1:11300"])
+    {:ok, _pid} = Watcher.Poller.start_link(:test_pool)
     :ok = GenEvent.add_mon_handler(StatsEventManager, Watcher.PollerTest.Forwarder, self)
     {:ok, []}
   end
