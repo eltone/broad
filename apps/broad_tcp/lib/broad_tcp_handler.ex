449fdd34b48e98df2096473befe598ec69cf4eb1
eltone <anthonyhobbs@gmail.com>
2016-12-02 17:47:55 +0000

    spike for supporting use and peek commands

--- apps/broad_tcp/lib/broad_tcp_handler.ex
+++ apps/broad_tcp/lib/broad_tcp_handler.ex
@@ -31,29 +31,40 @@ defmodule BroadTcp.Handler do
     end
   end
 
-  @supported_commands ~w(stats stats-tube delete peek peek-ready peek-delayed peek-buried kick
+  @supported_commands ~w(use stats stats-tube delete peek peek-delayed peek-buried kick
     kick-job stats-job list-tubes list-tube-used list-tubes-watched)
 
   defp parse([cmd | _opts] = input) when cmd in @supported_commands do
-    yaml = input
+    input
     |> Watcher.Pool.cmd
     |> format
-    line_with_yaml = ["---\n", yaml]
-    res_length = IO.iodata_length(line_with_yaml)
-    ["OK ", to_string(res_length), "\r\n", line_with_yaml, "\r\n"]
   end
 
   defp parse(_input) do
     "UNKNOWN_COMMAND\r\n"
   end
 
-  defp format(enum) when is_map(enum) do
-    enum
-    |> Enum.map(fn ({k,v}) -> [k, ": ", to_string(v), "\n"] end)
+  defp format({:message, string}), do: [string, "\r\n"]
+
+  defp format({:job, {id, body}}) do
+    ["FOUND ", to_string(id), " ", to_string(byte_size(body)), "\r\n", body, "\r\n"]
   end
 
-  defp format(enum) when is_list(enum) do
+  defp format({:yaml, enum}) when is_map(enum) do
+    enum
+    |> Enum.map(fn ({k,v}) -> [k, ": ", to_string(v), "\n"] end)
+    |> header
+  end
+
+  defp format({:yaml, enum}) when is_list(enum) do
     enum
     |> Enum.map(fn (x) -> ["- ", to_string(x), "\n"] end)
+    |> header
+  end
+
+  defp header(output) do
+    line_with_yaml = ["---\n", output]
+    res_length = IO.iodata_length(line_with_yaml)
+    ["OK ", to_string(res_length), "\r\n", line_with_yaml, "\r\n"]
   end
 end
--- apps/watcher/lib/watcher/pool.ex
+++ apps/watcher/lib/watcher/pool.ex
@@ -9,10 +9,17 @@ defmodule Watcher.Pool do
     GenServer.start_link(__MODULE__, beanstalks, [name: name])
   end
 
+  defmodule State do
+    @moduledoc """
+    Genserver state
+    """
+    defstruct beanstalks: [], using: "default", peek_buffer: []
+  end
+
   def init(beanstalks) do
     pids = beanstalks
     |> Enum.map(&Watcher.Beanstalk.parse_and_connect/1)
-    {:ok, %{beanstalks: pids}}
+    {:ok, %State{beanstalks: pids}}
   end
 
   # Client
@@ -28,12 +35,26 @@ defmodule Watcher.Pool do
 
   @aggregate_commands ~w(stats stats-tube)
   @union_commands ~w(list-tubes list-tubes-watched)
+  @buffered_commands ~w(peek-delayed peek-buried)
 
-  def handle_call({:cmd, cmd}, _from, %{beanstalks: pids} = state) do
+  def handle_call({:cmd, ["use", tube]}, _from, %State{beanstalks: pids} = state) do
+    pids
+    |> Enum.each(&ElixirTalk.use(&1, tube))
+    {:reply, {:message, "USING #{tube}"}, %{state | using: tube, peek_buffer: []}}
+  end
+
+  def handle_call({:cmd, [cmd | _opts]}, _from, state) when cmd in @buffered_commands do
+    {head, new_state} = peek(state, cmd)
+    # TODO: handle case where tube is empty
+    {:found, id, body} = head
+    {:reply, {:job, {id, body}}, new_state}
+  end
+
+  def handle_call({:cmd, cmd}, _from, %State{beanstalks: pids} = state) do
     agg_stats = pids
     |> Enum.map(&ElixirTalk.Connect.call(&1, format_args(cmd)))
     |> aggregate(cmd)
-    {:reply, agg_stats, state}
+    {:reply, {:yaml, agg_stats}, state}
   end
 
   defp format_args([cmd | []]), do: String.to_atom(cmd)
@@ -45,5 +66,17 @@ defmodule Watcher.Pool do
 
   defp aggregate(enum, [cmd | _opts]) when cmd in @union_commands do
     Watcher.Aggregator.union(enum)
+  end
+
+  defp peek(%State{beanstalks: pids, peek_buffer: []} = state, cmd) do
+    [head | _] = res = fetch(cmd, pids)
+    {head, %{state | peek_buffer: res}}
+  end
+
+  defp peek(%State{peek_buffer: [head | _]} = state, _cmd), do: {head, state}
+
+  defp fetch(cmd, pids) do
+    pids
+    |> Enum.map(&ElixirTalk.Connect.call(&1, format_args([cmd])))
   end
 end
